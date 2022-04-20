495afc4d44094a5c86695497450c2e60d2235597
Adal Zanabria <adal.zanabria@gmail.com>
2022-04-20 16:07:19 -0500

    Agregados nuevos archivos.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,12 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar
-polybar --config=~/.config/polybar/config.ini myBar 2>&1 | tee -a /tmp/polybar1.log & disown
+if [[ $(xrandr -q | grep 'HDMI-2 connected') ]];
+then
+polybar --config=~/.config/polybar/config.ini barLeft 2>&1 | tee -a /tmp/polybar1.log & disown &
+polybar --config=~/.config/polybar/config.ini barRight 2>&1 | tee -a /tmp/polybar2.log & disown &
+else
+polybar --config=~/.config/polybar/config.ini barLeft 2>&1 | tee -a /tmp/polybar1.log & disown &
+fi
 
 echo "Bar launched..."
