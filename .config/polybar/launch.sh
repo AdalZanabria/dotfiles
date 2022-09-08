f2e658d663b947138c305b2af6263af9390bd48c
AdalZanabria <adal.zanabria@gmail.com>
2022-09-07 22:22:05 -0500

    - Switched display config to a 4k and a FHD one. - Increased kitty opacity. - Current distro: Arch Linux. - Added condition for single or dual monitor for bspwm and polybar. - Added new modules to polybar.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+intern=eDP-1-1
+extern=HDMI-0
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
@@ -7,12 +9,13 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar
-if [[ $(xrandr -q | grep 'HDMI-2 connected') ]];
-then
-polybar --config=~/.config/polybar/config.ini barLeft 2>&1 | tee -a /tmp/polybar1.log & disown &
-polybar --config=~/.config/polybar/config.ini barRight 2>&1 | tee -a /tmp/polybar2.log & disown &
+if xrandr | grep "$extern connected"; then
+  echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+  polybar -r --config=~/.config/polybar/config.ini barLeft 2>&1 | tee -a /tmp/polybar1.log & disown &
+  polybar -r --config=~/.config/polybar/config.ini barRight 2>&1 | tee -a /tmp/polybar2.log & disown &
+  echo "Bar launched..."
 else
-polybar --config=~/.config/polybar/config.ini barLeft 2>&1 | tee -a /tmp/polybar1.log & disown &
+  echo "---" | tee -a /tmp/polybar1.log
+  polybar -r --config=~/.config/polybar/config.ini barSingle 2>&1 | tee -a /tmp/polybar1.log & disown &
+  echo "Bar launched..."
 fi
-
-echo "Bar launched..."
